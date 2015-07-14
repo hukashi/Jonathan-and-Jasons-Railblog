dfc5250622072ef3fcff35aec0fc9d0058dae690
Jason Guzik & Jonathan Chung <pair+billkozby83.hukashi@devbootcamp.com>
2015-07-14 11:07:08 -0400

    init create bcyrpt migrate

---
+++ JJblog/db/seeds.rb
@@ -0,0 +1,4 @@
+User.create(username:"jguzik", first_name:"Jason", last_name:"Guzik", password: '12345')
+User.create(username:'hukashi', first_name:'Jonathan', last_name:'Chung', password: 'yolo')
+
+
